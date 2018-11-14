e8c2a32fb5ce59645933ef309025fe0715bfde99
Leadbetter <llleadbe@88e9fe5d5262.ant.amazon.com>
2018-11-14 13:19:36 -0800

    logic to update schedule automatically from stored config/update config

--- public/js/config.js
+++ public/js/config.js
@@ -11,12 +11,33 @@ twitch.onContext((context) => {
 twitch.onAuthorized((auth) => {
   token = auth.token;
   userId = auth.userId;
+
 });
 
+// when the config changes, update the schedule! 
+twitch.configuration.onChanged(function(){
+  twitch.rig.log(twitch.configuration.broadcaster)
+  if(twitch.configuration.broadcaster){
+    try{
+      var config = JSON.parse(twitch.configuration.broadcaster.content)
+      console.log(typeof config)
+      if(typeof config === "object"){
+        events = config
+        updateSchedule()
+      }else{
+        console.log('invalid config')
+      }
+    }catch(e){
+      console.log('invalid config')
+    }
+  }
+})
+
+function updateConfig(){
+  twitch.configuration.set('broadcaster', '1', JSON.stringify(events))
+}
+
 $(function(){
-
-
-
   $("#date-form").submit(function(e){
     var title = $("#title").val()
     var description = $("#description").val()
