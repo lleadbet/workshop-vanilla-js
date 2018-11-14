797a14f9753af9d7f88bee6bd8183083271b3341
Leadbetter <llleadbe@88e9fe5d5262.ant.amazon.com>
2018-11-14 13:00:44 -0800

    first step (also applying some changes to start)

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,7 @@
 var token, userId;
 
+var events = []
+
 const twitch = window.Twitch.ext;
 
 twitch.onContext((context) => {
@@ -11,3 +13,44 @@ twitch.onAuthorized((auth) => {
   userId = auth.userId;
 });
 
+function updateSchedule(){
+  //empty the schedule
+  $("#schedule").empty();
+
+  // stores the overall data
+  var htmlArray = "";
+
+  for(i=0;i<events.length;i++){
+    var innerHtml = ""
+    for(key in events[i]){
+      innerHtml += "<p>"+key+": "+events[i][key]+"</p>";
+    }
+    htmlArray += "<div class=\"container\">"+innerHtml+"</div><hr />";
+  }
+
+  // now update with the correct data!
+  $("#schedule").append(htmlArray)
+}
+
+$(function(){
+
+
+
+  $("#date-form").submit(function(e){
+    var title = $("#title").val()
+    var description = $("#description").val()
+    var date = $("#date").val()
+
+    var data = {
+      Title:title,
+      Description:description,
+      Date: date    
+    }
+
+    events.push(data)
+    updateSchedule()
+
+    // return false to stop the default action and stop propogation as well
+    return false;
+  })
+})
