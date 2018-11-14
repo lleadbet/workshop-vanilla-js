68c5d3fabc646ab8f586b5f257385c4471cd2276
Leadbetter <llleadbe@88e9fe5d5262.ant.amazon.com>
2018-11-14 13:22:02 -0800

    adding rendering logic

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,5 +1,6 @@
 var token = "";
 var tuid = "";
+var events = [];
 
 // because who wants to type this every time?
 var twitch = window.Twitch.ext;
@@ -19,5 +20,19 @@ twitch.onAuthorized(function(auth) {
 $(function() {
     twitch.configuration.onChanged(function (){
         twitch.rig.log('got a configuration from config service!');
+        if(twitch.configuration.broadcaster){
+            try{
+              var config = JSON.parse(twitch.configuration.broadcaster.content)
+              console.log(typeof config)
+              if(typeof config === "object"){
+                events = config
+                updateSchedule()
+              }else{
+                console.log('invalid config')
+              }
+            }catch(e){
+              console.log('invalid config')
+            }
+          }
     });
 });
